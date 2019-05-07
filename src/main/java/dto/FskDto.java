a868fc74f478156bbc607e08618152f4410b40a4
Behzad Nikbin <behzad.nikbin@gmail.com>
2019-05-07 22:33:39 +0430

    equal hashcode for FskDto

--- src/main/java/dto/FskDto.java
+++ src/main/java/dto/FskDto.java
@@ -2,10 +2,7 @@ package dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -15,6 +12,7 @@ import java.util.List;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@EqualsAndHashCode(of = "asset_id")
 public class FskDto {
     private Long asset_id;
     private List<String> fsk_level_list_facet;
