d79a3a3dc52c75e7189c3c6f8fa9bef07eff33a9
Behzad Nikbin <behzad.nikbin@gmail.com>
2019-05-07 22:52:54 +0430

    main function

--- src/main/java/ForsterFskMain.java
+++ src/main/java/ForsterFskMain.java
@@ -1,5 +1,54 @@
+import dto.FskDto;
+import logic.FskDataProcessor;
+import lombok.extern.java.Log;
+import utils.SerializationUtils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+/**
+ * @author Behzad
+ */
 public class ForsterFskMain {
+
+    private static final Logger logger = Logger.getLogger(ForsterFskMain.class.getName());
+
     public static void main(String[] args) {
-        
+        final String INPUT_FILE_PATH = "assets.json";
+        final String OUTPUT_FILE_PATH = "output.json";
+
+        List<FskDto> list = null;
+        //  opening file
+        try (FileInputStream inStream = new FileInputStream(INPUT_FILE_PATH)) {
+            //  deserializing json from stream to FskDto list
+            list = SerializationUtils.instance.deserializeFromFile(inStream);
+        } catch (FileNotFoundException e) {
+            logger.severe("Input file not found at " + INPUT_FILE_PATH);
+        } catch (IOException e) {
+            logger.severe("Deserialization problem for " + INPUT_FILE_PATH);
+        }
+        if (list != null) {
+            logger.info("File " + INPUT_FILE_PATH + " opened successfully.");
+            //  grouping data
+            SortedMap<String, List<FskDto>> map = FskDataProcessor.instance.getFskGroupByLevelFacet(list);
+            Set<FskDto> fskSet = map.values().stream().flatMap(Collection::stream).collect(Collectors.toSet());
+            logger.info(fskSet.size() + " data items is grouped to " + map.keySet().size());
+            //  serializing to file
+            try (FileOutputStream outStream = new FileOutputStream(OUTPUT_FILE_PATH)) {
+                SerializationUtils.instance.serializeToStream(map, outStream);
+            } catch (FileNotFoundException e) {
+                logger.severe("Cannot write output file at " + OUTPUT_FILE_PATH);
+            } catch (IOException e) {
+                logger.severe("Cannot serialize to json file at " + OUTPUT_FILE_PATH);
+            }
+        }
     }
 }
