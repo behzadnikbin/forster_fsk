b99b12723f772261078c584d6e934255561d2a01
Behzad Nikbin <behzad.nikbin@gmail.com>
2019-05-07 22:34:24 +0430

    ObjectMapper pretty output

--- src/main/java/utils/SerializationUtils.java
+++ src/main/java/utils/SerializationUtils.java
@@ -3,20 +3,25 @@ package utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import dto.FskDto;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.SortedMap;
 
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SerializationUtils {
     public static final SerializationUtils instance = new SerializationUtils();
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private SerializationUtils() {
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
 
     public List<FskDto> deserializeFromFile(InputStream stream) throws IOException {
         return objectMapper.readValue(stream, new TypeReference<List<FskDto>>() {
@@ -25,5 +30,9 @@ public class SerializationUtils {
 
     public String serializeToSting(SortedMap<String, List<FskDto>> map) throws JsonProcessingException {
         return objectMapper.writeValueAsString(map);
+    }
+
+    public void serializeToStream(SortedMap<String, List<FskDto>> map, OutputStream outputStream) throws IOException {
+        objectMapper.writeValue(outputStream, map);
     }
 }
